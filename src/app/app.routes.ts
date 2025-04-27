a5a832704bb9a4256d1001e747555292f9a00fd8
Daria Poliakova <dashacovic@gmail.com>
2025-04-28 01:41:41 +0300

    R-14 finish header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { EventsComponent } from './events/events.component';
+import { CoachesComponent } from './coaches/coaches.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainComponent,
-    children: [],
+    children: [
+      { path: 'home', component: MainPageComponent },
+      { path: 'events', component: EventsComponent   },
+      { path: 'coaches', component: CoachesComponent  },
+      { path: 'about-us', component: AboutUsComponent  },
+    ],
   },
 ];
